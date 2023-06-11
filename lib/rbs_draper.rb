6ba6483ce6108a17892d707a76a7d64ddeafc192
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-06-11 20:12:13 +0900

    Load rbs_draper/*.rb automatically

--- lib/rbs_draper.rb
+++ lib/rbs_draper.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require_relative "rbs_draper/decoratable"
+require_relative "rbs_draper/decorator"
+require_relative "rbs_draper/rake_task"
 require_relative "rbs_draper/version"
 
 module RbsDraper
